bd008fd35e13b2e70026e9f6191a188199592be6
JarnoTiainen <tiainenjarno@gmail.com>
2021-09-21 14:51:58 +0300

    int i = 0;

--- Assets/Scripts/gitTest.cs
+++ Assets/Scripts/gitTest.cs
@@ -7,7 +7,7 @@ public class gitTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int i = 0;
     }
 
     // Update is called once per frame
