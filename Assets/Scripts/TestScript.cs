733156fc4b87b1ca43ef137d67a3bd64abd337a3
kaarlagd <max.gun.kaarla@gmail.com>
2021-09-22 11:05:42 +0300

    new tooltip test

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -12,6 +12,7 @@ public class TestScript : MonoBehaviour
     {
         GameEventManager.CallintThisMethod();
         Tooltip.ShowTooltip_Static("This is testrun for tooltips <sdf <dsf ffsdf <sdfeeee refds< d<f<<<<<<<<<< asd asd sdsa asd asd asd  sdsdsds sd sdasd <<<<<<<< dfdsf   asdas asd asd  asdaa dsdsd asd asd asd das asdas asdasda asdas aasdasda  asda asda aasdas  sdfsdfs      sdfsdffefes sdfww.", "now also with Title!");
+        Tooltip.ShowTooltip_Static("This is tooltip2 text hehe!", "otsikko");
     }
 
     // Update is called once per frame
