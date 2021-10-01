30167b04fec10410ab092805beb4ab140f396701
GGeru <56148582+GGeru@users.noreply.github.com>
2021-10-01 14:23:20 +0300

    modularized cardmovement and added test
    added test for card movement in kerttutest scene and made the cardmovement script more modular, so it can be used to do any kind of moving

--- Assets/Scripts/Animations/CardMovement.cs
+++ Assets/Scripts/Animations/CardMovement.cs
@@ -4,47 +4,56 @@ using UnityEngine;
 
 public class CardMovement : MonoBehaviour
 {
-    public GameObject discardGO;
-    public Transform cardTrans;
-    public Transform deckTrans;
-    public Transform discardTrans;
     public float duration = 2;
 
+    //specifies the movement curve of the card
+    [SerializeField]
+    private AnimationCurve curve;
+    private AnimationCurve defaultCurve;
     private Vector3 startPoint;
     private Vector3 endPoint;
     private float elapsedTime;
     private bool doMove = false;
 
-    //specifies the movement of the card
-    [SerializeField]
-    private AnimationCurve curve;
 
-    // Start is called before the first frame update
     void Start()
-    {
-        cardTrans = GameObject.FindGameObjectWithTag("Card").transform;
-        deckTrans = GameObject.FindGameObjectWithTag("Deck").transform;
-        discardTrans = GameObject.FindGameObjectWithTag("DiscardPile").transform;
-        discardGO = GameObject.FindGameObjectWithTag("DiscardPile");
-
-
+    {  
+        //this is so that we can set the used curve back to the default, if the curve value has been changed
+        defaultCurve = curve;
         
-        startPoint = cardTrans.position;
-        //getting the second transform in this list because otherwise it would return original transform 
-        endPoint = discardGO.GetComponentsInChildren<Transform>()[1].position;
+        startPoint = transform.position;
+        endPoint = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            doMove = true;
-        }
-
-        if(doMove && cardTrans.position != endPoint) {
+        if(doMove && transform.position != endPoint) {
             elapsedTime += Time.deltaTime;
-            //smoothly goes towards the endpoint. should this be in the fixedUpdate?
-            cardTrans.position = Vector3.Lerp(startPoint, endPoint, curve.Evaluate(elapsedTime / duration));
+            //smoothly moves towards the endpoint. should this be in the fixedUpdate?
+            transform.position = Vector3.Lerp(startPoint, endPoint, curve.Evaluate(elapsedTime / duration));
+        } else if (doMove && transform.position == endPoint) {  //if the card has moved to the destination, reset variables
+            doMove = false;
+            curve = defaultCurve;
+            elapsedTime = 0;
         }
+    }
+
+    //uses a specified animation curve
+    public void OnCardMove(Vector3 endP, float dur, AnimationCurve curv)
+    {
+        startPoint = transform.position;
+        endPoint = endP;
+        duration = dur;
+        curve = curv;
+        doMove = true;
+    }
+
+    //uses the default animation curve of card
+    public void OnCardMove(Vector3 endP, float dur)
+    {
+        startPoint = transform.position;
+        endPoint = endP;
+        duration = dur;
+        doMove = true;
     }
 }
