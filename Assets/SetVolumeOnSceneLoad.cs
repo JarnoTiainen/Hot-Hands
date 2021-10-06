e3e049dfa8954283759d2ba0f49d7d51a712a648
JarnoTiainen <tiainenjarno@gmail.com>
2021-10-06 19:57:53 +0300

    Load volume settings when launching straight from mainGame scene

---
+++ Assets/SetVolumeOnSceneLoad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolumeOnSceneLoad : MonoBehaviour
+{
+    private AudioMixer masterMixer;
+    [SerializeField] private float masterDefaultVolume = 0.5f;
+    [SerializeField] private float musicDefaultVolume = 0.5f;
+    [SerializeField] private float sfxDefaultVolume = 0.5f;
+
+    void Start()
+    {
+        masterMixer = Resources.Load("MasterMixer") as AudioMixer;
+        masterMixer.SetFloat("masterVol", Mathf.Log10(PlayerPrefs.GetFloat("MasterVolume", masterDefaultVolume)) * 20);
+        masterMixer.SetFloat("mainMenuMusicVol", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume", musicDefaultVolume)) * 20);
+        masterMixer.SetFloat("inGameMusicVol", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume", musicDefaultVolume)) * 20);
+        masterMixer.SetFloat("sfxVol", Mathf.Log10(PlayerPrefs.GetFloat("SFXVolume", sfxDefaultVolume)) * 20);
+    }
+
+
+}
