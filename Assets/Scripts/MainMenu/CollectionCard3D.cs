2ff0565c3ecfce67856fc6f3f000f288fae09700
JarnoTiainen <tiainenjarno@gmail.com>
2021-11-25 00:52:36 +0200

    Collection card attack directions

--- Assets/Scripts/MainMenu/CollectionCard3D.cs
+++ Assets/Scripts/MainMenu/CollectionCard3D.cs
@@ -7,16 +7,46 @@ public class CollectionCard3D : MonoBehaviour
 {
     public Card card;
     [SerializeField] private MeshRenderer meshRendererImage;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI costText;
+    [SerializeField] private TextMeshProUGUI rpText;
+    [SerializeField] private TextMeshProUGUI lpText;
+    [SerializeField] private TextMeshProUGUI valueText;
+    [SerializeField] private GameObject healthLeftGO;
+    [SerializeField] private GameObject healthRightGO;
+    [SerializeField] private GameObject powerLeftGO;
+    [SerializeField] private GameObject powerRightGO;
+
 
     private void Start()
     {
-        Transform cardCanvas = gameObject.transform.Find("Canvas");
-        cardCanvas.Find("CardName").GetComponent<TextMeshProUGUI>().text = card.cardName;
-        cardCanvas.Find("Cost").GetComponent<TextMeshProUGUI>().text = card.cost.ToString();
-        cardCanvas.Find("RP").GetComponent<TextMeshProUGUI>().text = card.rp.ToString();
-        cardCanvas.Find("LP").GetComponent<TextMeshProUGUI>().text = card.lp.ToString();
-        cardCanvas.Find("Value").GetComponent<TextMeshProUGUI>().text = card.value.ToString();
+        nameText.text = card.cardName;
+        costText.text = card.cost.ToString();
+        rpText.text = card.rp.ToString();
+        lpText.text = card.lp.ToString();
+        valueText.text = card.value.ToString();
         meshRendererImage.material.SetTexture("_CardImage", card.cardSprite.texture);
+        SetAttackDirectionSymbol();
+    }
+
+    public void SetAttackDirectionSymbol()
+    {
+        if (card.attackDirection == Card.AttackDirection.Left)
+        {
+            powerLeftGO.SetActive(true);
+            healthRightGO.SetActive(true);
+        }
+        else if (card.attackDirection == Card.AttackDirection.Right)
+        {
+            powerRightGO.SetActive(true);
+            healthLeftGO.SetActive(true);
+
+        }
+        if (card.cardType == Card.CardType.Spell)
+        {
+            rpText.gameObject.SetActive(false);
+            lpText.gameObject.SetActive(false);
+        }
     }
 
     public void AddCard()
