5d2f2bbd0200dac4b04db7de6a6f010ed2ae68c9
JarnoTiainen <tiainenjarno@gmail.com>
2021-12-06 03:08:11 +0200

    End screen animation

--- Assets/MatchResultScript.cs
+++ Assets/MatchResultScript.cs
@@ -1,20 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Sirenix.OdinInspector;
 
 public class MatchResultScript : MonoBehaviour
 {
     public static MatchResultScript Instance { get; private set; }
     [SerializeField] private GameObject resultScreenButtons;
+    [SerializeField] private Image panelDim;
+    [SerializeField] private TextMeshProUGUI resultText;
+    [SerializeField] private int dimMax;
+    [SerializeField] private int dimEnd;
+    [SerializeField] private float dimMaxFade;
+    [SerializeField] private float dimMaxDuration;
+    [SerializeField] private float dimEndFade;
 
     private void Awake()
     {
         Instance = this;
+        StartCoroutine(DimAnimation());
     }
 
-    public void ResultScreenButtonsSetActive(bool value)
+    public void ResultScreenButtonsSetActive(bool value) => resultScreenButtons.SetActive(value);
+
+    public IEnumerator DimAnimation()
     {
-        resultScreenButtons.SetActive(value);
+        float time = 0;
+        while (time < dimMaxFade)
+        {
+            time += Time.deltaTime;
+            float dim = Mathf.Lerp(0, dimMax, time / dimMaxFade);
+            panelDim.color = new Color32(0, 0, 0, (byte)Mathf.FloorToInt(dim));
+            yield return null;
+        }
+        resultText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(dimMaxDuration);
+        resultScreenButtons.SetActive(true);
+        time = 0;
+        while(time < dimEndFade)
+        {
+            time += Time.deltaTime;
+            float dim = Mathf.Lerp(dimMax, dimEnd, time / dimEndFade);
+            panelDim.color = new Color32(0, 0, 0, (byte)Mathf.FloorToInt(dim));
+            yield return null;
+        }
     }
 
+    [Button]
+    public void AnimationDevButton()
+    {
+        resultText.gameObject.SetActive(false);
+        resultScreenButtons.SetActive(false);
+        StartCoroutine(DimAnimation());
+    }
 }
