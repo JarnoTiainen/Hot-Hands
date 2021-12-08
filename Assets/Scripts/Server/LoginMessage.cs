e4c8791867e0712beb4196752e0a5e7194e08cfb
JarnoTiainen <tiainenjarno@gmail.com>
2021-12-08 22:51:24 +0200

    Login works now

--- Assets/Scripts/Server/LoginMessage.cs
+++ Assets/Scripts/Server/LoginMessage.cs
@@ -1,15 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LoginMessage : MonoBehaviour
+public class LoginMessage
 {
     public string username;
     public bool admin;
-
-    public LoginMessage(string username, bool admin)
-    {
-        this.username = username;
-        this.admin = admin;
-    }
 }
