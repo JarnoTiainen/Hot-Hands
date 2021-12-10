3bf192598aeb401df5e2d2ed869b5bab7e356b24
kaarlagd <max.gun.kaarla@gmail.com>
2021-12-10 05:32:03 +0200

    some fixes

--- Assets/Scripts/CardScripts/Animations/CardMovement.cs
+++ Assets/Scripts/CardScripts/Animations/CardMovement.cs
@@ -297,7 +297,16 @@ public class CardMovement : MonoBehaviour
                 int owner = GetComponent<InGameCard>().owner;
                 //shake the camera
                 ah.CameraShake(this.GetComponent<InGameCard>().GetData());
-                Vector3 impactEffectPos = endAttackPoint + (targetCard.transform.position - endAttackPoint).normalized * Vector3.Distance(targetCard.transform.position,endAttackPoint) * 0.5f;
+                Vector3 impactEffectPos;
+                if (targetCard == null)
+                {
+                    impactEffectPos = transform.position;
+                }
+                else
+                {
+                    impactEffectPos = endAttackPoint + (targetCard.transform.position - endAttackPoint).normalized * Vector3.Distance(targetCard.transform.position, endAttackPoint) * 0.5f;
+                }
+                
                 ah.SpawnImpactEffect(impactEffectPos + new Vector3(0,0,-0.03f));
                 GetComponent<InGameCard>().ToggleTrails(false);
                 if(targetCard.GetComponent<InGameCard>() != null) targetCard.GetComponent<InGameCard>().ToggleTrails(false);
