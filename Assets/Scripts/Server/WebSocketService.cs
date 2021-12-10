9e617b02ae2bfe7eceddc504137fbc32e0b09ee4
JarnoTiainen <tiainenjarno@gmail.com>
2021-12-10 05:32:38 +0200

    Minor changes

--- Assets/Scripts/Server/WebSocketService.cs
+++ Assets/Scripts/Server/WebSocketService.cs
@@ -215,7 +215,7 @@ public class WebSocketService : MonoBehaviour
                         if (debuggerModeOn) Debug.LogError("MESSAGE WAS UNKOWN: " + data[0] + " " + data[1]);
                         if(SceneManager.GetActiveScene().buildIndex == 0)
                         {
-                            MainMenu.Instance.CreatePopupNotification("Internal server error! Try restarting the game.", MainMenu.PopupCorner.BottomLeft, MainMenu.PopupTone.Negative);
+                            MainMenu.Instance.CreatePopupNotification("Internal server error! AWS might have problems.", MainMenu.PopupCorner.BottomLeft, MainMenu.PopupTone.Negative);
                         }
                         break;
                 }
